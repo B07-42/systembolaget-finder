e9ba687b0cc016a29fb365db8f154d9ac7378dfc
B07-42 <mattias.raubling@gmail.com>
2026-06-10 13:15:55 +0200

    Add proxy handler for API requests

---
+++ api/proxy.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
+  
+  const response = await fetch(decodeURIComponent(url), {
+    headers: {
+      'Ocp-Apim-Subscription-Key': '8d39a7340ee7439f8b4c1e995c8f3e4a'
+    }
+  });
+  
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json(data);
+}
